74c5fff49ddbdb21acd9ea2236b009e088674456
Daniel Kellar <daniel.kellar@hotmail.com>
2015-02-27 14:59:42 +0000

    still nothing

---
+++ assignment_2/a2q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int countInversion(const std::vector<int> & vec, unsigned start, unsigned end)
+{
+    if (end - start == 1) return 0;
+}
+
+int main()
+{
+}
